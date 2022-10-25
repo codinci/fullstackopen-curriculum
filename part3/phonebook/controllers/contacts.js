035dc8a794743c0e60957a85b0c7987220bc85f1
codinci <dkibau@gmail.com>
2022-10-25 09:26:14 +0300

    refractored controllers/contact.js using async/await

--- part3/phonebook/controllers/contacts.js
+++ part3/phonebook/controllers/contacts.js
@@ -1,26 +1,21 @@
 const contactsRouter = require('express').Router();
 const Contacts = require('../models/contact');
 
-contactsRouter.get('/', (request, response) => {
-	Contacts.find({}).then((contact) => {
-		console.log(contact);
+contactsRouter.get('/', async (request, response) => {
+	const contacts = await Contacts.find({});
+	response.json(contacts.map((contact) => contact.toJSON()));
+});
+
+contactsRouter.get('/:id', async (request, response) => {
+	const contact = await Contacts.findById(request.params.id);
+	if (contact) {
 		response.json(contact);
-	});
+	} else {
+		response.status(404).end;
+	}
 });
 
-contactsRouter.get('/:id', (request, response, next) => {
-	Contacts.findById(request.params.id)
-		.then((contact) => {
-			if (contact) {
-				response.json(contact);
-			} else {
-				response.status(404).end();
-			}
-		})
-		.catch((error) => next(error));
-});
-
-contactsRouter.put('/:id', (request, response, next) => {
+contactsRouter.put('/:id', async (request, response) => {
 	const body = request.body;
 
 	const person = {
@@ -28,39 +23,36 @@ contactsRouter.put('/:id', (request, response, next) => {
 		number: body.number,
 	};
 
-	Contacts.findByIdAndUpdate(request.params.id, person, {
-		new: true,
-		runValidators: true,
-		context: 'query',
-	})
-		.then((updatedContact) => {
-			response.json(updatedContact);
-		})
-		.catch((error) => next(error));
+	const updatedContact = await Contacts.findByIdAndUpdate(
+		request.params.id,
+		person,
+		{
+			new: true,
+			runValidators: true,
+			context: 'query',
+		}
+	);
+	if (updatedContact) {
+		response.json(updatedContact);
+	} else {
+		response.status(404).end();
+	}
 });
 
-contactsRouter.post('/', (request, response, next) => {
+contactsRouter.post('/', async (request, response) => {
 	const body = request.body;
 
 	const person = new Contacts({
 		name: body.name,
-		number: body.number
+		number: body.number,
 	});
-
-	person
-		.save()
-		.then((savedContact) => {
-			response.json(savedContact);
-		})
-		.catch((error) => next(error));
+	const savedContact = await person.save();
+	response.status(201).json(savedContact);
 });
 
-contactsRouter.delete('/:id', (request, response, next) => {
-	Contacts.findByIdAndRemove(request.params.id)
-		.then(() => {
-			response.status(204).end();
-		})
-		.catch((error) => next(error));
+contactsRouter.delete('/:id', async (request, response) => {
+	await Contacts.findByIdAndRemove(request.params.id);
+	response.status(204).end;
 });
 
 module.exports = contactsRouter;
--- part4/blog-list/controller/blogs.js
+++ part4/blog-list/controller/blogs.js
@@ -1,19 +1,20 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
 });
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post("/", async (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    
-    response.status(201).json(result);
-  });
+  try {
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = blogsRouter;
