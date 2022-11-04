e28e03e2fef1660a084a48b4ed538b3da4ea650e
codinci <dkibau@gmail.com>
2022-11-04 19:10:59 +0300

    learning redux

---
+++ part4/blog-list/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es2021': true,
+		'jest': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'no-console': 0,
+	}
+}
